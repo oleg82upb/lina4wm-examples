ecc65bd08efeacfac1bb4b8f627f3ea969641a69
oleg82@zitmail.uni-paderborn.de <oleg82@zitmail.uni-paderborn.de@3d81485f-1b0b-62ed-d016-0b1ab8ae057d>
2014-12-22 14:22:00 +0000

    - bugfixing dekker - recompiled since the optimizations don't work for dekker

--- Dekker Mutex/dekker.c
+++ Dekker Mutex/dekker.c
@@ -1,46 +1,47 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-bool flag[2] = {0,0};
-int turn = 0;
+bool *flag0 = 0;
+bool *flag1 = 0;
+bool *turn = 0;
 
 void p0()
 {
-	flag[0] = 1;
-	while (flag[1])
+	*flag0 = 1;
+	while (*flag1)
 	{
-		if (turn != 0)
+		if (*turn != 0)
 		{
-			flag[0] = 0;
-			while (turn != 0)
+			*flag0 = 0;
+			while (*turn != 0)
 			{
 				// busy wait
 			}
-			flag[0] = 1;
+			*flag0 = 1;
 		}
 	}
 
 	// critical section
-	turn = 1;
-	flag[0] = 0;
+	*turn = 1;
+	*flag0 = 0;
 }
 
 void p1()
 {
-	flag[1] = 1;
-	   while (flag[0]) {
-	      if (turn != 1) {
-	         flag[1] = 0;
-	         while (turn != 1) {
+	*flag1 = 1;
+	   while (*flag0) {
+	      if (*turn != 1) {
+	         *flag1 = 0;
+	         while (*turn != 1) {
 	           // busy wait
 	         }
-	         flag[1] = 1;
+	         *flag1 = 1;
 	      }
 	   }
 
 	   // critical section
-	   turn    = 0;
-	   flag[1] = 0;
+	   *turn    = 0;
+	   *flag1 = 0;
 }
 
 int main () {
--- Dekker Mutex/dekker.s
+++ Dekker Mutex/dekker.s
@@ -2,74 +2,121 @@
 target datalayout = "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:32:64-f32:32:32-f64:32:64-v64:64:64-v128:128:128-a0:0:64-f80:32:32-n8:16:32-S128"
 target triple = "i386-pc-linux-gnu"
 
-@flag = global [2 x i8] zeroinitializer, align 1
-@turn = global i32 0, align 4
+@flag0 = global i8* null, align 4
+@flag1 = global i8* null, align 4
+@turn = global i8* null, align 4
 
 define void @p0() nounwind {
 entry:
-  store i8 1, i8* getelementptr inbounds ([2 x i8]* @flag, i32 0, i32 0), align 1, !tbaa !0
-  %0 = load i8* getelementptr inbounds ([2 x i8]* @flag, i32 0, i32 1), align 1, !tbaa !0, !range !3
-  %1 = and i8 %0, 1
-  %tobool5 = icmp eq i8 %1, 0
-  br i1 %tobool5, label %while.end4, label %while.body.lr.ph
+  %0 = load i8** @flag0, align 4
+  store i8 1, i8* %0, align 1
+  fence seq_cst
+  br label %while.cond
 
-while.body.lr.ph:                                 ; preds = %entry
-  %2 = load i32* @turn, align 4, !tbaa !4
-  %cmp = icmp eq i32 %2, 0
-  br i1 %cmp, label %while.cond.backedge.us.us, label %if.then.if.then.split_crit_edge
+while.cond:                                       ; preds = %if.end, %entry
+  %1 = load i8** @flag1, align 4
+  %2 = load i8* %1, align 1
+  %tobool = trunc i8 %2 to i1
+  %conv = zext i1 %tobool to i32
+  %cmp = icmp eq i32 %conv, 1
+  br i1 %cmp, label %while.body, label %while.end12
 
-while.cond.backedge.us.us:                        ; preds = %while.cond.backedge.us.us, %while.body.lr.ph
-  br label %while.cond.backedge.us.us
+while.body:                                       ; preds = %while.cond
+  %3 = load i8** @turn, align 4
+  %4 = load i8* %3, align 1
+  %tobool2 = trunc i8 %4 to i1
+  %conv3 = zext i1 %tobool2 to i32
+  %cmp4 = icmp ne i32 %conv3, 0
+  br i1 %cmp4, label %if.then, label %if.end
 
-if.then.if.then.split_crit_edge:                  ; preds = %while.body.lr.ph
-  store i8 0, i8* getelementptr inbounds ([2 x i8]* @flag, i32 0, i32 0), align 1, !tbaa !0
-  br label %while.cond1
+if.then:                                          ; preds = %while.body
+  %5 = load i8** @flag0, align 4
+  store i8 0, i8* %5, align 1
+  br label %while.cond6
 
-while.cond1:                                      ; preds = %while.cond1, %if.then.if.then.split_crit_edge
-  br label %while.cond1
+while.cond6:                                      ; preds = %while.body11, %if.then
+  %6 = load i8** @turn, align 4
+  %7 = load i8* %6, align 1
+  %tobool7 = trunc i8 %7 to i1
+  %conv8 = zext i1 %tobool7 to i32
+  %cmp9 = icmp ne i32 %conv8, 0
+  br i1 %cmp9, label %while.body11, label %while.end
 
-while.end4:                                       ; preds = %entry
-  store i32 1, i32* @turn, align 4, !tbaa !4
-  store i8 0, i8* getelementptr inbounds ([2 x i8]* @flag, i32 0, i32 0), align 1, !tbaa !0
+while.body11:                                     ; preds = %while.cond6
+  br label %while.cond6
+
+while.end:                                        ; preds = %while.cond6
+  %8 = load i8** @flag0, align 4
+  store i8 1, i8* %8, align 1
+  br label %if.end
+
+if.end:                                           ; preds = %while.end, %while.body
+  br label %while.cond
+
+while.end12:                                      ; preds = %while.cond
+  %9 = load i8** @turn, align 4
+  store i8 1, i8* %9, align 1
+  %10 = load i8** @flag0, align 4
+  store i8 0, i8* %10, align 1
   ret void
 }
 
 define void @p1() nounwind {
 entry:
-  store i8 1, i8* getelementptr inbounds ([2 x i8]* @flag, i32 0, i32 1), align 1, !tbaa !0
-  %0 = load i8* getelementptr inbounds ([2 x i8]* @flag, i32 0, i32 0), align 1, !tbaa !0, !range !3
-  %1 = and i8 %0, 1
-  %tobool5 = icmp eq i8 %1, 0
-  br i1 %tobool5, label %while.end4, label %while.body.lr.ph
+  %0 = load i8** @flag1, align 4
+  store i8 1, i8* %0, align 1
+  fence seq_cst
+  br label %while.cond
 
-while.body.lr.ph:                                 ; preds = %entry
-  %2 = load i32* @turn, align 4, !tbaa !4
-  %cmp = icmp eq i32 %2, 1
-  br i1 %cmp, label %while.cond.backedge.us, label %if.then.if.then.split_crit_edge
+while.cond:                                       ; preds = %if.end, %entry
+  %1 = load i8** @flag0, align 4
+  %2 = load i8* %1, align 1
+  %tobool = trunc i8 %2 to i1
+  %conv = zext i1 %tobool to i32
+  %cmp = icmp eq i32 %conv, 1
+  br i1 %cmp, label %while.body, label %while.end12
 
-while.cond.backedge.us:                           ; preds = %while.cond.backedge.us, %while.body.lr.ph
-  br label %while.cond.backedge.us
+while.body:                                       ; preds = %while.cond
+  %3 = load i8** @turn, align 4
+  %4 = load i8* %3, align 1
+  %tobool2 = trunc i8 %4 to i1
+  %conv3 = zext i1 %tobool2 to i32
+  %cmp4 = icmp ne i32 %conv3, 1
+  br i1 %cmp4, label %if.then, label %if.end
 
-if.then.if.then.split_crit_edge:                  ; preds = %while.body.lr.ph
-  store i8 0, i8* getelementptr inbounds ([2 x i8]* @flag, i32 0, i32 1), align 1, !tbaa !0
-  br label %while.cond1
+if.then:                                          ; preds = %while.body
+  %5 = load i8** @flag1, align 4
+  store i8 0, i8* %5, align 1
+  br label %while.cond6
 
-while.cond1:                                      ; preds = %while.cond1, %if.then.if.then.split_crit_edge
-  br label %while.cond1
+while.cond6:                                      ; preds = %while.body11, %if.then
+  %6 = load i8** @turn, align 4
+  %7 = load i8* %6, align 1
+  %tobool7 = trunc i8 %7 to i1
+  %conv8 = zext i1 %tobool7 to i32
+  %cmp9 = icmp ne i32 %conv8, 1
+  br i1 %cmp9, label %while.body11, label %while.end
 
-while.end4:                                       ; preds = %entry
-  store i32 0, i32* @turn, align 4, !tbaa !4
-  store i8 0, i8* getelementptr inbounds ([2 x i8]* @flag, i32 0, i32 1), align 1, !tbaa !0
+while.body11:                                     ; preds = %while.cond6
+  br label %while.cond6
+
+while.end:                                        ; preds = %while.cond6
+  %8 = load i8** @flag1, align 4
+  store i8 1, i8* %8, align 1
+  br label %if.end
+
+if.end:                                           ; preds = %while.end, %while.body
+  br label %while.cond
+
+while.end12:                                      ; preds = %while.cond
+  %9 = load i8** @turn, align 4
+  store i8 0, i8* %9, align 1
+  %10 = load i8** @flag1, align 4
+  store i8 0, i8* %10, align 1
   ret void
 }
 
-define i32 @main() nounwind readnone {
+define i32 @main() nounwind {
 entry:
   ret i32 0
 }
-
-!0 = metadata !{metadata !"_Bool", metadata !1}
-!1 = metadata !{metadata !"omnipotent char", metadata !2}
-!2 = metadata !{metadata !"Simple C/C++ TBAA", null}
-!3 = metadata !{i8 0, i8 2}                       
-!4 = metadata !{metadata !"int", metadata !1}
