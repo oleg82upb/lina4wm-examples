9b340f3e895856693d1f4b22479b602081a28a2d
alexander.hetzer@gmail.com <alexander.hetzer@gmail.com@3d81485f-1b0b-62ed-d016-0b1ab8ae057d>
2015-01-15 18:06:34 +0000

    changed testcases

--- SimpleTests/LoopTests/Test_Dependent_Write_Read_2_1_cmpxchLoop.s
+++ SimpleTests/LoopTests/Test_Dependent_Write_Read_2_1_cmpxchLoop.s
@@ -2,10 +2,10 @@ define void @_Test() nounwind readonly {
 entry:
   %b = add i32 5, 0
   %r1 = alloca i32
-  store i32 %b, i32* %r1
-  %0 = load i32* %r1, align 4, !tbaa !0
   %a = add i32 1, 0
 loop:
+  store i32 %b, i32* %r1
+  %0 = load i32* %r1, align 4, !tbaa !0
   %val_success = cmpxchg i32* %r1, i32 %b, i32 %a seq_cst
   %plus = add i32 %a, %b
   %cond = icmp eq i32 %a, %b
--- SimpleTests/LoopTests/Test_Dependent_Write_Read_2_1_noSynch.s
+++ SimpleTests/LoopTests/Test_Dependent_Write_Read_2_1_noSynch.s
@@ -2,10 +2,11 @@ define void @_Test() nounwind readonly {
 entry:
   %b = add i32 5, 0
   %r1 = alloca i32
-  store i32 %b, i32* %r1
-  %0 = load i32* %r1, align 4, !tbaa !0
+  
   %a = add i32 1, 0
 loop:
+  store i32 %b, i32* %r1
+  %0 = load i32* %r1, align 4, !tbaa !0
   %cond = icmp eq i32 %a, %b
   %a = add i32 %a, 1
   br i1 %cond, label %return, label %loop
